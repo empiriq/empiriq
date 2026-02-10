6c5fc28bc9b4c7ba17fca8f76bec0d4f6038aae6
Mikhail Gerasimov <migerasimoff@gmail.com>
2026-02-10 16:46:26 +0100

    chore: subscribers example

--- demo/src/EventSubscriber/MetricCollector.php
+++ demo/src/EventSubscriber/MetricCollector.php
@@ -6,41 +6,45 @@ use Empiriq\BinanceContracts\Derivatives\FuturesUsdM\Events\Market\TradeEvent;
 use Empiriq\BinanceContracts\Derivatives\FuturesUsdM\Events\User\AccountUpdateEvent;
 use Empiriq\BinanceContracts\Derivatives\FuturesUsdM\Events\User\OrderTradeUpdateEvent;
 use Prometheus\CollectorRegistry;
-use PrometheusPushGateway\PushGateway;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 
-readonly class Metrics implements EventSubscriberInterface
+readonly class MetricCollector implements EventSubscriberInterface
 {
     public function __construct(
         private CollectorRegistry $registry,
-        private PushGateway $gateway
     ) {
     }
 
     public static function getSubscribedEvents(): array
     {
         return [
+            'ticker.tick.1s' => 'handleTicker',
             TradeEvent::class => 'handleTrade',
             OrderTradeUpdateEvent::class => 'handleOrderTradeUpdate',
             AccountUpdateEvent::class => 'handleAccountUpdateEvent',
         ];
     }
 
+    public function handleTicker(object $event): void
+    {
+        var_dump('handleTicker');
+        var_dump('handleTicker');
+        var_dump('handleTicker');
+        var_dump('handleTicker');
+    }
+
     public function handleTrade(TradeEvent $event): void
     {
         $this->registry->getOrRegisterCounter('market_events', 'trade_count', 'help')->inc();
-        $this->gateway->push($this->registry, 'trade_workers');
     }
 
     public function handleOrderTradeUpdate(OrderTradeUpdateEvent $event): void
     {
         $this->registry->getOrRegisterCounter('user_events', 'order_trade_update_count', 'help')->inc();
-        $this->gateway->push($this->registry, 'trade_workers');
     }
 
     public function handleAccountUpdateEvent(AccountUpdateEvent $event): void
     {
         $this->registry->getOrRegisterCounter('user_events', 'account_update_count', 'help')->inc();
-        $this->gateway->push($this->registry, 'trade_workers');
     }
 }
