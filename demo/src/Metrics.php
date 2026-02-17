09c3ced5b75fe8ce4166df9931814321d5a9b9b0
Mikhail Gerasimov <migerasimoff@gmail.com>
2026-02-18 00:02:19 +0100

    refactor: demo app metrics

---
+++ demo/src/Metrics.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App;
+
+use Empiriq\BinanceContracts\Derivatives\FuturesUsdM\Events\Market\TradeEvent;
+use Prometheus\CollectorRegistry;
+use PrometheusPushGateway\PushGateway;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+
+readonly class Metrics implements EventSubscriberInterface
+{
+    public function __construct(
+        private CollectorRegistry $registry,
+        private PushGateway $gateway
+    ) {
+    }
+
+    public static function getSubscribedEvents(): array
+    {
+        return [
+            'binance.futures_usd.market.trade?symbol=btcusdt' => 'trade',
+            'ticker.tick?interval=5s' => 'push',
+        ];
+    }
+
+    public function trade(TradeEvent $event): void
+    {
+        $this->registry->getOrRegisterCounter('market_events', 'trade_count', 'Trade count')->inc();
+    }
+
+    public function push(): void
+    {
+        $this->gateway->push($this->registry, 'trade_workers');
+    }
+}
