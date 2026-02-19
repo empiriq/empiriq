1c8a81b21485ce330d23f2b38fff86f5513d13d8
Mikhail Gerasimov <migerasimoff@gmail.com>
2026-02-19 08:42:32 +0100

    refactor: remove unused config builder end call

--- src/BinanceTradeBundle/DependencyInjection/Configuration.php
+++ src/BinanceTradeBundle/DependencyInjection/Configuration.php
@@ -69,8 +69,7 @@ final class Configuration implements ConfigurationInterface
                             ->end()
                         ->end()
                     ->end()
-                ->end()
-            ->end();
+                ->end();
 
         return $treeBuilder;
     }
